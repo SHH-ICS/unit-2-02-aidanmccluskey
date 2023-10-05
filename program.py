fb7d87675e806bc962fb8117e2c080717d22af3c
aidanmccluskey <144241853+aidanmccluskey@users.noreply.github.com>
2023-10-05 09:00:08 -0400

    Update program.py

--- program.py
+++ program.py
@@ -2,6 +2,6 @@
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
 myfunction(name):
-myfunction = "Hello" + Adizzy
+def myfunction = "Hello" + Adizzy
 return(myfunction)
 print(myfuctuion("world"))
